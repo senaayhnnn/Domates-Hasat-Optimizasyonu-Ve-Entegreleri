0871f2a897ee20dbcc1a5ffb55f2777381d5648c
Senanur <ayhansena215@gmail.com>
2025-08-25 17:23:59 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for chatbottarimproject project.
+URL configuration for web project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from Chatbottarim import views
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
-
+from django.urls import path,include 
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
+    path('',include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('chat', views.chat, name='chat'),
-    path('agroculus/', views.agroculus_view, name='agroculus'),  # buraya ekledik
-    path('', TemplateView.as_view(template_name='index.html')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
